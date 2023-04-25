8abf070c70f178beee00326b975d741c0330a05c
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-04-25 23:04:29 +0100

    fix

--- .history/src/screens/TrackListScreen_20230418114533.js
+++ .history/src/screens/TrackListScreen_20230418114533.js
@@ -5,9 +5,10 @@ const TrackListScreen = ({navigation}) => {
   return (
     <>
       <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
-      <Button
-        title="Go to Track Detail"
-        onPress={() => navigation.navigate("TrackDetail")}
+      <Button 
+      title="Go to Track Detail" 
+      onPress={()=> navigation.navigate('TrackDetail')}
+      
       />
     </>
   );
